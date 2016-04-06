5642811daa43bbd980419cc426112685ce8ac9f9
mrg <blacknext@gmail.com>
2016-04-06 18:07:18 -0400

    Moved ObjEntity Attributes Tab contents to separate FXML/Controller.  Fixes #3.

--- src/main/java/org/apache/cayenne/modeler/layout/ObjectEntityLayout.java
+++ src/main/java/org/apache/cayenne/modeler/layout/ObjectEntityLayout.java
@@ -21,46 +21,24 @@ package org.apache.cayenne.modeler.layout;
 
 import java.io.IOException;
 
-import org.apache.cayenne.map.ObjAttribute;
 import org.apache.cayenne.map.ObjEntity;
-import org.apache.cayenne.modeler.utility.ObjectEntityUtilities;
 
-import de.jensd.fx.glyphs.GlyphsDude;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 public class ObjectEntityLayout extends AnchorPane implements MainWindowSupport
 {
     @FXML
-    private TableView<ObjAttribute> attributesTableView;
-
-    @FXML
-    private TableColumn<ObjAttribute,String> attributeNameColumn;
-
-    @FXML
-    private TableColumn<ObjAttribute,String> attributeTypeColumn;
-//    private TableColumn<ObjAttribute,ComboBox<String>> attributeTypeColumn;
-
-    @FXML
-    private TableColumn<ObjAttribute,Boolean> attributeUsedForLockingColumn;
-
-    @FXML
-    private TableColumn<ObjAttribute,Boolean> attributeIsInheritedColumn;
-
-    @FXML
-    private AnchorPane classTabAnchorPane;
+    private AnchorPane
+        classTabAnchorPane,
+        attributesTabAnchorPane;
 
     private MainWindowLayout mainWindow;
 
-    private ObjectEntityClassTabLayout objectEntityClassTabViewController;
+    private ObjectEntityClassTabLayout objectEntityClassTabLayout;
+    private ObjectEntityAttributesTabLayout objectEntityAttributesTabLayout;
 
     public ObjectEntityLayout(MainWindowLayout mainWindow) throws IOException
     {
@@ -73,63 +51,22 @@ public class ObjectEntityLayout extends AnchorPane implements MainWindowSupport
         fxmlLoader.load();
     }
 
-    private static ObservableList javaTypes = FXCollections.observableArrayList(ObjectEntityUtilities.getRegisteredTypeNames());
-
     public void initialize()
     {
-//        getScene().getWindow().getScene();
-//        getStage().getScene().getWindow().get
-//        System.out.println("mrg: " + getStage().getScene().getRoot());
         System.out.println("oev");
 
         loadComponents();
-
-        attributeUsedForLockingColumn.setText(null);
-        attributeIsInheritedColumn.setText(null);
-
-        attributeUsedForLockingColumn.setGraphic(GlyphsDude.createIcon(FontAwesomeIcon.LOCK, "16px"));
-        attributeIsInheritedColumn.setGraphic(GlyphsDude.createIcon(FontAwesomeIcon.LEVEL_UP, "16px"));
-
-        attributeNameColumn.setCellValueFactory(new PropertyValueFactory("name"));
-//        attributeTypeColumn.setCellValueFactory(new PropertyValueFactory("type"));
-        attributeTypeColumn.setCellValueFactory(new PropertyValueFactory<ObjAttribute, String>("type"));
-
-//        Callback<TableColumn<ObjAttribute, String>, TableCell<ObjAttribute, String>> comboBoxCellFactory
-//        = (TableColumn<ObjAttribute, String> param) -> new ComboBoxEditingCell();
-//
-//        ComboBoxTableCell attributeTypeCell = new ComboBoxTableCell(javaTypes);
-
-//        attributeTypeColumn.setCellFactory(attributeTypeCell);
-////        attributeTypeColumn.setCellFactory(ComboBoxTableCell.forTableColumn(ObjectEntityUtilities.getRegisteredTypeNames()));
-//        attributeTypeColumn.setEditable(true);
-////        attributeTypeColumn.set
-//        attributeTypeColumn.setOnEditCommit(
-//                        new EventHandler<CellEditEvent<ObjAttribute, String>>() {
-//                            @Override
-//                            public void handle(CellEditEvent<ObjAttribute,String> t) {
-//                                System.out.println(t);
-////                                ((ObjAttribute) t.getTableView().getItems().get(t.getTablePosition().getRow())).setLevel(t.getNewValue());
-//                            }
-//                        });
-
-//        attributeTypeColumn.setPromptText("Java Type");
-//        emailComboBox.setEditable(true);
-//        emailComboBox.valueProperty().addListener(new ChangeListener<String>() {
-//            @Override
-//            public void changed(ObservableValue ov, String t, String t1) {
-//                address = t1;
-//            }
-//        });
-
     }
 
     private void loadComponents()
     {
         try
         {
-            objectEntityClassTabViewController = new ObjectEntityClassTabLayout(this);
+            objectEntityClassTabLayout      = new ObjectEntityClassTabLayout(this);
+            objectEntityAttributesTabLayout = new ObjectEntityAttributesTabLayout(this);
 
-            loadTab(objectEntityClassTabViewController, classTabAnchorPane);
+            loadTab(objectEntityClassTabLayout, classTabAnchorPane);
+            loadTab(objectEntityAttributesTabLayout, attributesTabAnchorPane);
         }
         catch (Exception exception)
         {
@@ -155,8 +92,8 @@ public class ObjectEntityLayout extends AnchorPane implements MainWindowSupport
     public void display(ObjEntity objEntity)
     {
         System.out.println("trying to display: " + objEntity);
-        attributesTableView.setItems(FXCollections.observableArrayList(objEntity.getAttributes()));
-        objectEntityClassTabViewController.display(objEntity);
+        objectEntityClassTabLayout.display(objEntity);
+        objectEntityAttributesTabLayout.display(objEntity);
 //        objEntity.getAttributes()
     }
 
