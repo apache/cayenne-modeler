12a8c7745df1b56f7881f8e8baf6f8afc5442a6f
mrg <blacknext@gmail.com>
2016-12-06 08:58:22 -0500

    Streamlined binding methods and added JavaDoc.

--- src/main/java/org/apache/cayenne/modeler/adapters/CayennePropertyAdapter.java
+++ src/main/java/org/apache/cayenne/modeler/adapters/CayennePropertyAdapter.java
@@ -25,47 +25,94 @@ import org.apache.commons.logging.LogFactory;
 
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.Property;
 import javafx.beans.property.StringProperty;
 import javafx.beans.property.adapter.JavaBeanBooleanPropertyBuilder;
 import javafx.beans.property.adapter.JavaBeanIntegerPropertyBuilder;
 import javafx.beans.property.adapter.JavaBeanStringPropertyBuilder;
 
+/**
+ * Provides a common mechanism for creating property adapters. Property adapters
+ * map existing Java/Beans classes to JavaFX properties which can be used in
+ * bi-directional bindings within the UI.
+ */
 public abstract class CayennePropertyAdapter
 {
     private static final Log LOGGER = LogFactory.getLog(CayennePropertyAdapter.class);
 
+    /**
+     * Binds a Java/Bean String property to a JavaFX property. Note: The JavaFX
+     * property observes changes and marks the Cayenne project dirty when a
+     * change occurs.
+     *
+     * @param property
+     *            The Java/Bean property to bind to.
+     * @return A new JavaFX property bound to the Java/Bean property.
+     * @throws NoSuchMethodException
+     *             If the specified Java/Bean property does not exist (check the
+     *             property's spelling).
+     */
     public BooleanProperty bindBoolean(String property) throws NoSuchMethodException
     {
-        BooleanProperty boundProperty = JavaBeanBooleanPropertyBuilder.create().bean(getWrappedObject()).name(property).build();
-
-        boundProperty.addListener((observable, newValue, oldValue) -> markProjectDirty());
-
-        return boundProperty;
+        return observePropertyChanges(JavaBeanBooleanPropertyBuilder.create().bean(getWrappedObject()).name(property).build());
     }
 
+    /**
+     * Binds a Java/Bean String property to a JavaFX property. Note: The JavaFX
+     * property observes changes and marks the Cayenne project dirty when a
+     * change occurs.
+     *
+     * @param property
+     *            The Java/Bean property to bind to.
+     * @return A new JavaFX property bound to the Java/Bean property.
+     * @throws NoSuchMethodException
+     *             If the specified Java/Bean property does not exist (check the
+     *             property's spelling).
+     */
     public IntegerProperty bindInteger(String property) throws NoSuchMethodException
     {
-        IntegerProperty boundProperty = JavaBeanIntegerPropertyBuilder.create().bean(getWrappedObject()).name(property).build();
-
-        boundProperty.addListener((observable, newValue, oldValue) -> markProjectDirty());
-
-        return boundProperty;
+        return observePropertyChanges(JavaBeanIntegerPropertyBuilder.create().bean(getWrappedObject()).name(property).build());
     }
 
+    /**
+     * Binds a Java/Bean String property to a JavaFX property. Note: The JavaFX
+     * property observes changes and marks the Cayenne project dirty when a
+     * change occurs.
+     *
+     * @param property
+     *            The Java/Bean property to bind to.
+     * @return A new JavaFX property bound to the Java/Bean property.
+     * @throws NoSuchMethodException
+     *             If the specified Java/Bean property does not exist (check the
+     *             property's spelling).
+     */
     public StringProperty bindString(String property) throws NoSuchMethodException
     {
-        StringProperty boundProperty = JavaBeanStringPropertyBuilder.create().bean(getWrappedObject()).name(property).build();
-
-        boundProperty.addListener((observable, newValue, oldValue) -> markProjectDirty());
-
-        return boundProperty;
+        return observePropertyChanges(JavaBeanStringPropertyBuilder.create().bean(getWrappedObject()).name(property).build());
     }
 
-    private void markProjectDirty()
+    /**
+     * Observes changes to the given property and marks the project dirty
+     * (edited) when the property is changed.
+     *
+     * @param property
+     *            The property to observe changes.
+     * @return The observed property.
+     */
+    private <T extends Property<?>> T observePropertyChanges(T property)
     {
-        getCayennePropject().setDirty(true);
+        property.addListener((observable, newValue, oldValue) -> getCayennePropject().setDirty(true));
+
+        return property;
     }
 
+    /**
+     * @return The Cayenne project this property adapter is attached to.
+     */
     public abstract CayenneProject getCayennePropject();
+
+    /**
+     * @return The Cayenne Java/Bean object this property adapter wraps.
+     */
     public abstract Object getWrappedObject();
 }
